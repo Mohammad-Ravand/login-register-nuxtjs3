981b6f17fa37253f90559e5642bd06930f805f38
webbakeryMohammad-Ravand <159992017+webbakeryMohammad-Ravand@users.noreply.github.com>
2025-01-08 11:21:55 +0330

    small change in home page

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
-  <h1>Home Page is here</h1>
+  <BRow>
+    <BCol>
+      <BCarousel  controls indicators ride="carousel">
+        <BCarouselSlide img-src="https://picsum.photos/1024/480/?image=4" />
+        <BCarouselSlide img-src="https://picsum.photos/1024/480/?image=5" />
+        <BCarouselSlide img-src="https://picsum.photos/1024/480/?image=6" />
+      </BCarousel>
+    </BCol>
+  </BRow>
+  <br>
+   <BCard class="border-0">
+     <BCardHeader>
+       <BCardTitle class="text-center">
+         Welcome To My Website
+       </BCardTitle>
+     </BCardHeader>
+   </BCard>
 </template>
 
 
